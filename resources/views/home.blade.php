c7260457c88d64ee0eace2c129b4281d7753611c
nblkmal <nblkmal@gmail.com>
2021-12-22 16:40:45 +0800

    add vue in blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>This is example from ItSolutionStuff.com</h1>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+</body>
+</html>
