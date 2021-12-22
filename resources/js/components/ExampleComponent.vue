c903f7514fa5d157b75b7fded863f6ecc9a94030
nblkmal <nblkmal@gmail.com>
2021-12-23 00:03:46 +0800

    update form input with route

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,12 +6,32 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        I'm an example component.{{ custom }}
+                        <input v-model="message" placeholder="edit me">
+                        <p>Message is: {{ message }}</p>
                         <div class="list-group">
-                            <a href="" class="list-group-item" v-for="product in products" :key="product">
+                            <a href="" class="list-group-item" v-for="product in products">
                                 {{ product.name }}
                             </a>
                         </div>
+                    </div>
+                    <div class="card-body">
+                        <form @submit.prevent="submit">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Name</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="name" v-model="fields.name" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Details</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="detail" v-model="fields.detail" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                            <button type="submit">Submit</button>
+                        </form>
+                        
                     </div>
                 </div>
             </div>
@@ -21,9 +41,15 @@
 
 <script>
     export default {
+
+        props: ['homeRoute'],
+
         data: function() {
             return {
                 products: [],
+                custom: 'helloooo',
+                message: '',
+                fields: {},
                 // loading: true
             }
         },
@@ -45,6 +71,17 @@
                 });
                 // assign
                 // catch errors
+            },
+            submit() {
+                // post to endpoint
+                axios.post( this.homeRoute, this.fields )
+                // axios.post('/api/products/store', this.fields)
+                .then(response => {
+                    this.fields = {};
+                    console.log(this.fields);
+                }).catch(error => {
+                    console.log(error);
+                })
             }
         }
     }
